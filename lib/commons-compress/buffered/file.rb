b2d627ca80bba778d87b23dc09ddefdb274d8e45
Pat Bair <p.bair@modcloth.com>
2013-07-05 15:38:28 -0400

    refactored buffered file

--- lib/commons-compress/buffered/file.rb
+++ lib/commons-compress/buffered/file.rb
@@ -1,56 +1,67 @@
-java_import java.io.FileInputStream
-java_import java.io.FileOutputStream
-java_import java.io.BufferedInputStream
-java_import java.io.BufferedOutputStream
-
 module Commons
   module Compress
     module Buffered
       class File
 
+        RDONLY = 0x1.freeze
+        WRONLY = 0x2.freeze
+
+        def self.open(filename, modestr, &block)
+          open_mode = parse(modestr)
+
+          if block.nil?
+            unsafe_open(filename, open_mode)
+          else
+            auto(filename, open_mode, &block)
+          end
+        end
+
         class << self
-          def open(filename, modestr, &block)
-            modes = parse(modestr)
-
-            if block.nil?
-              unsafe_open(filename, modes)
-            else
-              auto(filename, modes, &block)
-            end
-          end
           alias_method :new, :open
+        end
 
-          private
+        private
 
-          def parse(modestr)
-            { read: modestr == 'r', write: modestr == 'w' }
+        def self.parse(modestr)
+          case modestr
+          when 'r' then RDONLY
+          when 'w' then WRONLY
           end
+        end
 
-          def unsafe_open(filename, modes)
-            underlying_stream_klass = modes[:read] ? FileInputStream : FileOutputStream
-
-            BufferedStream.new(underlying_stream_klass.new(filename), modes)
+        def self.unsafe_open(filename, open_mode)
+          case open_mode
+          when RDONLY then unsafe_open_read(filename)
+          when WRONLY then unsafe_open_write(filename)
           end
+        end
 
-          def auto(filename, modes)
-            begin
-              buffered_stream = unsafe_open(filename, modes)
+        def self.auto(filename, modes)
+          begin
+            buffered_stream = unsafe_open(filename, modes)
 
-              yield buffered_stream
-            rescue Exception => e
-              raise e
-            ensure
-              buffered_stream.close unless buffered_stream.nil?
-            end
+            yield buffered_stream
+          rescue Exception => e
+            raise e
+          ensure
+            buffered_stream.close unless buffered_stream.nil?
           end
+        end
+
+        def self.unsafe_open_read(filename)
+          BufferedStream.new(java.io.FileInputStream.new(filename), RDONLY)
+        end
+
+        def self.unsafe_open_write(filename)
+          BufferedStream.new(java.io.FileOutputStream.new(filename), WRONLY)
         end
 
         class BufferedStream
           attr_reader :wrapped_stream
 
-          def initialize(underlying_stream, modes)
+          def initialize(underlying_stream, open_mode)
             @underlying_stream = underlying_stream
-            @wrapped_stream = wrapped_stream_klass(modes).new(@underlying_stream)
+            @wrapped_stream = open(open_mode)
           end
 
           def close
@@ -62,8 +73,19 @@ module Commons
 
           attr_reader :underlying_stream
 
-          def wrapped_stream_klass(modes)
-            modes[:read] ? BufferedInputStream : BufferedOutputStream
+          def open(open_mode)
+            case open_mode
+            when RDONLY then open_read
+            when WRONLY then open_write
+            end
+          end
+
+          def open_read
+            java.io.BufferedInputStream.new(underlying_stream)
+          end
+
+          def open_write
+            java.io.BufferedOutputStream.new(underlying_stream)
           end
         end
       end
